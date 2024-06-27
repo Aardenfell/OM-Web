f459d858e91069a056076857cb6ab436935d66fd
Aardenfell <40066656+Aardenfell@users.noreply.github.com>
2024-06-27 12:48:02 -0500

    feat: add support for new card design in timer.js

--- js/timers.js
+++ js/timers.js
@@ -9,6 +9,13 @@ const timers = [
   //     resetTime: "00:00", // Reset time in 24-hour format. Use an array if multiple times
   //     startTime: "09:00", // Start time in 24-hour format. Use if an event has a duration
   //     endTime: "22:00" // End time in 24-hour format. use if an event has a duration
+  //     eventDesc: "Lorem Ipsum", // Event description
+  //     image: "", // Image URL
+  //     listGroup: "Lorem Ipsum, Test1, Test2", // still broken
+  //     cardLink1Text: "Lorem Ipsum", // Link text
+  //     cardLink1Url: "https://www.google.com", // Link URL
+  //     cardLink2Text: "Lorem Ipsum",  // Link text
+  //     cardLink2Url: "https://www.google.com", // Link URL
   // },
   {
     name: "Daily Reset",
@@ -28,10 +35,18 @@ const timers = [
     resetTime: ["17:00", "23:00"], // Reset time in 24-hour format
   },
   {
-    name: "Weekly Event A",
+    name: "Placeholder Test",
     interval: "weekly", // Weekly timer
     resetDay: 1, // Monday
     resetTime: "12:00", // Reset time in 24-hour format
+    eventDesc: "Lorem Ipsum Lorem Ipsum   Lorem Ipsum", 
+    image: "",
+    listGroup: "Lorem Ipsum, Test1, Test2",
+    cardLink1Text: "Lorem Ipsum",
+    cardLink1: "https://www.google.com",
+    cardLink2Text: "Lorem Ipsum pt 2T",
+    cardLink2: "https://www.google.com",
+    
   },
   {
     name: "World Apex",
@@ -52,23 +67,5 @@ const timers = [
       startTime: "13:00", // Start time in 24-hour format. Use if an event has a duration
       endTime: "03:00" // End time in 24-hour format. use if an event has a duration
   },
-  // {
-  //     name: "", // Give it a name
-  //     interval: "daily, custom, weekly, hourly", // Choose between "daily", "weekly", "hourly", or "custom"
-  //     intervalHours: 1, // Only used if interval is "hourly". Can be any increment of hours.
-  //     resetDay: 0, // 0 = Sunday, 1 = Monday, etc. Only used if interval is 'weekly' or 'custom'. Use an array if multiple days
-  //     resetTime: "00:00", // Reset time in 24-hour format. Use an array if multiple times
-  //     startTime: "09:00", // Start time in 24-hour format. Use if an event has a duration
-  //     endTime: "22:00" // End time in 24-hour format. use if an event has a duration
-  // },
-  // {
-  //     name: "", // Give it a name
-  //     interval: "daily, custom, weekly, hourly", // Choose between "daily", "weekly", "hourly", or "custom"
-  //     intervalHours: 1, // Only used if interval is "hourly". Can be any increment of hours.
-  //     resetDay: 0, // 0 = Sunday, 1 = Monday, etc. Only used if interval is 'weekly' or 'custom'. Use an array if multiple days
-  //     resetTime: "00:00", // Reset time in 24-hour format. Use an array if multiple times
-  //     startTime: "09:00", // Start time in 24-hour format. Use if an event has a duration
-  //     endTime: "22:00" // End time in 24-hour format. use if an event has a duration
-  // },
   // Add more timers as needed
 ];
